40eebff6e07f6c6e59c10ecf0df3ca242ee4c935
rooooooooob <raftias@gmail.com>
2016-04-22 00:13:56 -0700

    added support for methods and std::function

--- test.cpp
+++ test.cpp
@@ -1,22 +1,21 @@
 #include <string>
 #include <iomanip>
 #include <iostream>
+#include <functional>
 
-struct Person
+//#define LCO_FRIEND_FUNC friend bool lco::less_than
+
+namespace lco
 {
-	std::string surname;
-	int age;
-	char initial;
-};
 
 template <typename Object, typename Member, typename... Members>
-bool less_than(const Object& lhs, const Object& rhs, Member member, Members... members)
+bool less_than(const Object& lhs, const Object& rhs, const Member& member, Members... members)
 {
-	if (lhs.*member < rhs.*member)
+	if (less_than(lhs, rhs, member))
 	{
 		return true;
 	}
-	else if (lhs.*member > rhs.*member)
+	else if (less_than(rhs, lhs, member))
 	{
 		return false;
 	}
@@ -24,14 +23,62 @@ bool less_than(const Object& lhs, const Object& rhs, Member member, Members... m
 }
 
 template <typename Object, typename Member>
-bool less_than(const Object& lhs, const Object& rhs, Member member)
+bool less_than(const Object& lhs, const Object& rhs, const Member& member)
 {
-	return (lhs.*member < rhs.*member);
+	return less_than_impl(lhs, rhs, member);
 }
+
+template <typename Object, typename MethodReturn>
+bool less_than_impl(const Object& lhs, const Object& rhs, MethodReturn(Object::*method)() const)
+{
+	return (lhs.*method)() < (rhs.*method)();
+}
+
+template <typename Object, typename MethodReturn>
+bool less_than_impl(const Object& lhs, const Object& rhs, const std::function<MethodReturn(const Object&)>& f)
+{
+	return f(lhs) < f(rhs);
+}
+
+template <typename Object, typename MemberType>
+bool less_than_impl(const Object& lhs, const Object& rhs, MemberType Object::*member)
+{
+	return (lhs.*member) < (rhs.*member);
+}
+
+} // lco
+
+
+class Person
+{
+public:
+	Person(std::string surname, int age, char initial)
+		:surname(std::move(surname))
+		, age(age)
+		, initial(initial)
+	{
+	}
+
+	int getAge() const
+	{
+		return age;
+	}
+
+	char getInitial() const
+	{
+		return initial;
+	}
+
+//private:
+	std::string surname;
+	int age;
+	char initial;
+};
 
 bool operator<(const Person& lhs, const Person& rhs)
 {
-	return less_than(lhs, rhs, &Person::surname, &Person::age, &Person::initial);
+	std::function<char(const Person& p)> f([](const Person& p) -> char {return p.getInitial();});
+	return lco::less_than(lhs, rhs, &Person::surname, &Person::getAge, f);
 }
 
 std::ostream& operator<<(std::ostream& os, const Person& p)
@@ -43,11 +90,11 @@ std::ostream& operator<<(std::ostream& os, const Person& p)
 int main()
 {
 	const Person people[5] = {
-		{ "Smith", 43, 'B' },
-		{ "Smith", 43, 'J' },
-		{ "Smith", 9, 'B' },
-		{ "Doe", 19, 'J' },
-		{ "Doe", 43, 'J' }
+		Person("Smith", 43, 'B'),
+		Person("Smith", 43, 'J'),
+		Person("Smith", 9, 'B'),
+		Person("Doe", 19, 'J'),
+		Person("Doe", 43, 'J')
 	};
 	for (const Person& lhs : people)
 	{
